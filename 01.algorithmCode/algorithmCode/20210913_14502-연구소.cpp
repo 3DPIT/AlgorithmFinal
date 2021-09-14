1413b63160e98313551fe218009db15a148a091e
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-09-14 23:36:48 +0900

    [21.09.14] 14500 테트로미노
    시뮬레이션 문제 포인트 dfs 가능한것은 dfs로 풀어라

--- 01.algorithmCode/algorithmCode/20210913_14502-연구소.cpp
+++ 01.algorithmCode/algorithmCode/20210913_14502-연구소.cpp
@@ -1,113 +1,135 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <string.h>
-#include <queue>
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+
 using namespace std;
-#define NSIZE 8
-#define MSIZE 8
-int wall[NSIZE][MSIZE];//벽, 공백, 바이러스 저장되는 배열
-int answer;//최대 결과갑 저장
-int N, M;
-struct Data {
-	int y, x;
-};
-vector<Data>virus;
+#define NS 504
+#define MS 504
+int N, M;//보드 입력으로 주는 가로 세로
+int maxAnswer;//최대 결과값 저장
+
+int board[NS][MS];// 테트로미노 놓는 보드
+int arr[NS][MS];// 테트로미노 그리는 보드
 int dy[] = { 0,1,0,-1 };
 int dx[] = { 1,0,-1,0 };
-void init();
-void dfs(int i, int j, int cnt);
-bool safe(int y, int x);//범위 체크
-//검증코드
-void checkWall(int a[NSIZE][MSIZE]); //제대로 벽세워지는지
-void checkVirus(vector<Data>v);//제대로 바이러스 뽑는지
+int ah[4][3][3] = {
+	{
+	{1,1,1},
+	{0,1,0},
+	{0,0,0}
+	},
+	{
+	{0,1,0},
+	{1,1,0},
+	{0,1,0}
+	},
+	{
+	{0,1,0},
+	{1,1,1},
+	{0,0,0}
+	},
+	{
+	{1,0,0},
+	{1,1,0},
+	{1,0,0}
+	}
+};
+
+void init();//초기화
+void play();//시뮬레이션 구현
+void dfs(int i, int j, int cnt);// 테트로미노 만드는 함수
+bool safe(int y, int x);// 범위 체크
+
+//체크 함수
+void checkBoard(int idx);//보드 체크
+
 int main(void) {
 	int testCase = 1;
 	for (int tc = 1; tc <= testCase; tc++) {
 		init();
-		dfs(0, 0, 0);//벽세우고 계산하기
-		//printf("#%d %d\n", tc, answer);
-		printf("%d\n", answer);
+		play();
+		printf("%d\n", maxAnswer);
 	}
 	return 0;
 }
 
-void dfs(int i, int j, int cnt) {
-	if (cnt == 3) {
-		//checkWall(wall);
-		//바이러스 검사후 안전영역 확인
-		queue<Data>q;
-		int visit[NSIZE][MSIZE] = { 0, };//방문체크
-		for (int i = 0; i < virus.size(); i++) {
-			q.push({ virus[i].y,virus[i].x });
-			visit[virus[i].y][virus[i].x] = 1;
-		}
-		while (!q.empty()) {
-			Data  c = q.front(); q.pop();
-			for (int dir = 0; dir < 4; dir++) {
-				Data n;
-				n.y = c.y + dy[dir];
-				n.x = c.x + dx[dir];
-				if (safe(n.y, n.x) && wall[n.y][n.x] == 0 && visit[n.y][n.x] == 0) {
-					visit[n.y][n.x] = 1;
-					q.push(n);
-				}
-			}
-		}
-		int cnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (wall[i][j] == 0 && visit[i][j] == 0) {
-					cnt++;
-				}
-			}
-		}
-		answer = answer < cnt ? cnt : answer;
-		return;
-	}
-	for (int y = i; y < N; y++) {
-		for (int x = j; x < M; x++) {
-			if (wall[y][x] == 0) {
-				wall[y][x] = -1;
-				dfs(y, x + 1, cnt + 1);
-				wall[y][x] = 0;
-			}
-		}
-		j = 0;
-	}
-}
 void init() {
-	answer = 0x80000000;
 	N = M = 0;
-	memset(wall, 0, sizeof(wall));
+	maxAnswer = 0x80000000;//최대값
+	memset(board, -1, sizeof(board));
 	scanf("%d %d", &N, &M);
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			scanf("%d", &wall[i][j]);
-			if (2 == wall[i][j]) {
-				virus.push_back({ i, j });//바이러스 저장
-			}
+			scanf("%d", &board[i][j]);
 		}
 	}
-	//checkVirus(virus);
+	//checkBoard(0);
 }
-
-bool safe(int y, int x) {//범위 체크
+bool safe(int y, int x) {
 	return 0 <= y && y < N && 0 <= x && x < M;
 }
-
-//검증코드
-void checkVirus(vector<Data>v) {
-	for (int i = 0; i < v.size(); i++) {
-		printf("%d %d\n", v[i].y, v[i].x);
+void dfs(int i, int j, long long int sum, int cnt) {
+	if (cnt == 3) {
+		//checkBoard(1);
+		//printf("\n");
+		maxAnswer = maxAnswer < sum ? sum : maxAnswer;
+		return;
+	}
+	for (int dir = 0; dir < 4; dir++) {
+		int ny = i + dy[dir]; int nx = j + dx[dir];
+		if (safe(ny, nx) && arr[ny][nx] == 0) {
+			arr[ny][nx] = 1;
+			dfs(ny, nx, sum + board[ny][nx], cnt + 1);
+			arr[ny][nx] = 0;
+		}
 	}
 }
-void checkWall(int a[NSIZE][MSIZE]) {
+
+void play() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			printf("%d ", a[i][j]);
+			arr[i][j] = 1;
+			dfs(i, j, board[i][j], 0);
+			arr[i][j] = 0;
 		}
-		printf("\n");
 	}
-	printf("\n");
+	//ㅏ,ㅓ,ㅗ ㅜ에 대해서 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+
+			for (int k = 0; k < 4; k++) {
+				long long int sum = 0;
+				for (int ii = 0; ii < 3; ii++) {
+					for (int jj = 0; jj < 3; jj++) {
+						int ny = i + ii;
+						int nx = j + jj;
+						if (ah[k][ii][jj] == 1) {
+							sum += board[ny][nx];
+						}
+					}//jj
+				}//ii
+				maxAnswer = maxAnswer < sum ? sum : maxAnswer;
+			}//k
+
+		}//j
+	}//i
 }
+void checkBoard(int idx) {
+	if (idx == 0) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				printf("%d ", board[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				printf("%d ", arr[i][j]);
+			}
+			printf("\n");
+		}
+	}
+}
+
---
+++ 01.algorithmCode/algorithmCode/20210914_14500-테트로미노.cpp
@@ -0,0 +1,137 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<string.h>
+
+using namespace std;
+#define NS 1004
+#define MS 1004
+int N, M;//보드 입력으로 주는 가로 세로
+int maxAnswer;//최대 결과값 저장
+
+int board[NS][MS];// 테트로미노 놓는 보드
+int arr[NS][MS];// 테트로미노 그리는 보드
+int dy[] = { 0,1,0,-1 };
+int dx[] = { 1,0,-1,0 };
+char ah[4][3][3] = {
+	{
+	{1,1,1},
+	{0,1,0},
+	{0,0,0}
+	},
+	{
+	{0,1,0},
+	{1,1,0},
+	{0,1,0}
+	},
+	{
+	{0,1,0},
+	{1,1,1},
+	{0,0,0}
+	},
+	{
+	{0,1,0},
+	{0,1,1},
+	{0,1,0}
+	}
+};
+
+void init();//초기화
+void play();//시뮬레이션 구현
+void dfs(int i, int j, int cnt);// 테트로미노 만드는 함수
+bool safe(int y, int x);// 범위 체크
+
+//체크 함수
+void checkBoard(int idx);//보드 체크
+
+int main(void) {
+	int testCase = 1;
+	for (int tc = 1; tc <= testCase; tc++) {
+		init();
+		play();
+		printf("%d\n", maxAnswer);
+	}
+	return 0;
+}
+
+void init() {
+	N = M = 0;
+	maxAnswer = 0x80000000;//최대값
+	memset(board,-1, sizeof(board));
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			scanf("%d", &board[i][j]);
+		}
+	}
+	//checkBoard(0);
+}
+bool safe(int y, int x) {
+	return 0 <= y && y < N && 0 <= x && x < M;
+}
+void dfs(int i, int j,long long sum, int cnt) {
+	if (cnt == 3) {
+		//checkBoard(1);
+		//printf("\n");
+		maxAnswer = maxAnswer < sum ? sum : maxAnswer;
+		return;
+	}
+	for (int dir = 0; dir < 4; dir++) {
+		int ny = i + dy[dir]; int nx = j + dx[dir];
+		if (safe(ny, nx) && arr[ny][nx] == 0) {
+			arr[ny][nx] = 1;
+			dfs(ny, nx,sum+board[ny][nx],cnt + 1);
+			arr[ny][nx] = 0;
+		}
+	}
+}
+
+void play() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			arr[i][j] = 1;
+			dfs(i, j,board[i][j], 0);
+			arr[i][j] = 0;
+		}
+	}
+	//ㅏ,ㅓ,ㅗ ㅜ에 대해서 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+
+			for (int k = 0; k < 4; k++) {
+				int sum = 0;
+				for (int ii = 0; ii < 3; ii++) {
+					for (int jj = 0; jj < 3; jj++) {
+						int ny = i + ii;
+						int nx = j + jj;
+						if (ah[k][ii][jj] == 1) {
+							if (board[ny][nx] == -1) sum += -100;
+							else sum += board[ny][nx];
+						}
+					}//jj
+				}//ii
+				maxAnswer = maxAnswer < sum ? sum : maxAnswer;
+			}//k
+
+		}//j
+	}//i
+}
+void checkBoard(int idx) {
+	if (idx == 0) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				printf("%d ", board[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				printf("%d ", arr[i][j]);
+			}
+			printf("\n");
+		}
+	}
+}
+
