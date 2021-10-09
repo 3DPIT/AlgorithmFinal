c6215bb067527a0e96617e753018693f38f1d7d7
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-10-10 02:42:28 +0900

    [21.10.10] docs: 20061-모노미노도미노2 문제 완료 및 수정

--- 01.algorithmCode/algorithmCode/20211009_20061-모노미노도미노2.cpp
+++ 01.algorithmCode/algorithmCode/20211009_20061-모노미노도미노2.cpp
@@ -101,58 +101,68 @@ void moveBlueZone(int t, int y, int x) {
 	}
 }
 void removeFourGreenBlock() {
-	for (int y = 9; y >= 6; y--) {
-		if (green[y][0] ==1&& green[y][1] ==1&& green[y][2] ==1&& green[y][3] == 1) {
-			green[y][0] = green[y][1] = green[y][2] = green[y][3] = 0;
-			score++;
-			for (int y2 = y; y2 >= 3; y2--) {
-				green[y2][0] = green[y2 - 1][0];
-				green[y2][1] = green[y2 - 1][1];
-				green[y2][2] = green[y2 - 1][2];
-				green[y2][3] = green[y2 - 1][3];
+	while (1) {
+		int flag = 1;
+		for (int y = 9; y >= 6; y--) {
+			if (green[y][0] == 1 && green[y][1] == 1 && green[y][2] == 1 && green[y][3] == 1) {
+				green[y][0] = green[y][1] = green[y][2] = green[y][3] = 0;
+				flag = 0;
+				score++;
+				for (int y2 = y; y2 >= 3; y2--) {
+					green[y2][0] = green[y2 - 1][0];
+					green[y2][1] = green[y2 - 1][1];
+					green[y2][2] = green[y2 - 1][2];
+					green[y2][3] = green[y2 - 1][3];
+				}
 			}
 		}
+		if (flag == 1)break;
 	}
 }
 void removeFourBlueBlock() {
-	for (int x = 9; x >= 6; x--) {
-		if (blue[0][x] ==1&&blue[1][x] ==1&& blue[2][x] ==1&& blue[3][x] == 1) {
-			blue[0][x] = blue[1][x] = blue[2][x] = blue[3][x] = 0;
-			score++;
-			for (int x2 = x; x2 >= 3; x2--) {
-				blue[0][x2] = blue[0][x2 - 1];
-				blue[1][x2] = blue[1][x2 - 1];
-				blue[2][x2] = blue[2][x2 - 1];
-				blue[3][x2] = blue[3][x2 - 1];
+	while (1) {
+		int flag = 1;
+		for (int x = 9; x >= 6; x--) {
+			if (blue[0][x] == 1 && blue[1][x] == 1 && blue[2][x] == 1 && blue[3][x] == 1) {
+				blue[0][x] = blue[1][x] = blue[2][x] = blue[3][x] = 0;
+				flag = 0;
+				score++;
+				for (int x2 = x; x2 >= 3; x2--) {
+					blue[0][x2] = blue[0][x2 - 1];
+					blue[1][x2] = blue[1][x2 - 1];
+					blue[2][x2] = blue[2][x2 - 1];
+					blue[3][x2] = blue[3][x2 - 1];
+				}
 			}
 		}
+		if (flag == 1)break;
 	}
 }
 void removeSpecialGreen() {
 	int cnt = 0;
-	if (green[4][0] == 1 || green[4][1] == 1 || green[4][2] == 1 || green[4][3] == 1)cnt++;
-	if (green[5][0] == 1 || green[5][1] == 1 || green[5][2] == 1 || green[5][3] == 1)cnt++;
-	if (cnt != 0) {
-		for (int y = 9; y >= 3; y--) {
-			green[y][0] = green[y - cnt][0];
-			green[y][1] = green[y - cnt][1];
-			green[y][2] = green[y - cnt][2];
-			green[y][3] = green[y - cnt][3];
+		if (green[4][0] == 1 || green[4][1] == 1 || green[4][2] == 1 || green[4][3] == 1)cnt++;
+		if (green[5][0] == 1 || green[5][1] == 1 || green[5][2] == 1 || green[5][3] == 1)cnt++;
+		if (cnt != 0) {
+			for (int y = 9; y >= 2; y--) {
+				green[y][0] = green[y - cnt][0];
+				green[y][1] = green[y - cnt][1];
+				green[y][2] = green[y - cnt][2];
+				green[y][3] = green[y - cnt][3];
+			}
 		}
-	}
 }
 void removeSpecialBlue() {
 	int cnt = 0;
-	if (blue[0][4] == 1 || blue[1][4] == 1 || blue[2][4] == 1 || blue[3][4] == 1)cnt++;
-	if (blue[0][5] == 1 || blue[1][5] == 1 || blue[2][5] == 1 || blue[3][5] == 1)cnt++;
-	if (cnt != 0) {
-		for (int x = 9; x >= 5; x--) {
-			blue[0][x] = blue[0][x - cnt];
-			blue[1][x] = blue[1][x - cnt];
-			blue[2][x] = blue[2][x - cnt];
-			blue[3][x] = blue[3][x - cnt];
+		if (blue[0][4] == 1 || blue[1][4] == 1 || blue[2][4] == 1 || blue[3][4] == 1)cnt++;
+		if (blue[0][5] == 1 || blue[1][5] == 1 || blue[2][5] == 1 || blue[3][5] == 1)cnt++;
+		if (cnt != 0) {
+			for (int x = 9; x >= 2; x--) {
+				blue[0][x] = blue[0][x - cnt];
+				blue[1][x] = blue[1][x - cnt];
+				blue[2][x] = blue[2][x - cnt];
+				blue[3][x] = blue[3][x - cnt];
+			}
 		}
-	}
 }
 void sumBlock() {
 	for (int y = 0; y <= 3; y++) {
