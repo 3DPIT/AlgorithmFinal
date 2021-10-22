29bf614ce4ec0aecc6bda63a13c5bbb870929c1e
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-10-22 22:00:38 +0900

    [21.10.22] docs: 11736-평범한숫자 풀이 진행
    sw expert D3난이도 마스터 힘내자

---
+++ 01.algorithmCode/algorithmCode/20211022_11736-평범한숫자.cpp
@@ -0,0 +1,41 @@
+
+#include<iostream>
+using namespace std;
+int N
+int answer;
+int arr[21];//숫자 저장
+void init();//초기화 및 초기 입력
+void normalNumber();//평범한 숫자 확인
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+
+	scanf("%d", &T);
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		init();
+		normalNumber();
+		printf("#%d %d\n", test_case, answer);
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
+
+void init() {
+	N = answer=0;
+	scanf("%d", &N);
+	for(int n = 0; n < N; n++) {
+		scanf("%d",&arr[n]);
+	}
+}
+void normalNumber() {
+	for (int i = 1; i < N - 1; i++) {
+		int  small = 0,big = 0;
+		if (arr[i - 1] < arr[i])small++;
+		if (arr[i + 1] < arr[i]) small++;
+		if (arr[i - 1] > arr[i])big++;
+		if (arr[i + 1] > arr[i]) big++;
+		if (small == 1 && big == 1)answer++;
+	}
+}
