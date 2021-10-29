fbdcf523279518a86501b591233cbe9657ba0a16
3DPIT <47946136+3DPIT@users.noreply.github.com>
2021-10-29 20:45:06 +0900

    [21.10.29] docs: 8658-Summation 문제 풀이, 문서 작성 진행

---
+++ 01.algorithmCode/algorithmCode/20211029_8658-Summation.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int maxNum, minNum;
+void initData();
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	
+	scanf("%d", &T);
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		initData();
+		printf("#%d %d\n",test_case,maxNum, minNum)
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
+void initData() {
+	maxNum = 0x80000000;
+	minNum = 0x7fffffff;
+	string firstNumber;
+	for (int i = 0; i < 10; i++) {
+		cin >> firstNumber;
+		int sumNumber = 0;
+		for (int i = 0; i < firstNumber.size(); i++) {
+			sumNumber += firstNumber[i] - '0';
+		}
+		if (sumNumber > maxNum)maxNum = sumNumber;
+		if (sumNumber < minNum)minNum = sumNumber;
+	}
+}
